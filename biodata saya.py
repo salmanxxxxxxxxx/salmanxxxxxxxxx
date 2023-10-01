55c3a17bb9eb5020a02405af86a49b16bb558814
5520123021 - SALMAN ALPARIZI <145079690+salmanxxxxxxxxx@users.noreply.github.com>
2023-10-01 14:51:44 +0800

    Add files via upload
    ajari saya supaya bisa

---
+++ biodata saya.py
@@ -0,0 +1,14 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('biodata saya')
+biodata saya
+>>> print('nama:salman alparizi')
+nama:salman alparizi
+>>> print('alamat:lebah munte')
+alamat:lebah munte
+>>> print('asal sekolah:SMK QAMARUL HUDA BAGU')
+asal sekolah:SMK QAMARUL HUDA BAGU
+>>> print('cita cita:DPR')
+cita cita:DPR
+>>> print('hoby:badminton')
+hoby:badminton
