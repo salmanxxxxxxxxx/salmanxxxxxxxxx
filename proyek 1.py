4ae0156f17aa4ae2f3a4d88f882aaf19046f2a66
5520123021 - SALMAN ALPARIZI <145079690+salmanxxxxxxxxx@users.noreply.github.com>
2023-10-15 23:47:52 +0800

    Add files via upload

---
+++ proyek 1.py
@@ -0,0 +1,12 @@
+A=7
+B=9
+hasilpenjumlahan=A+B
+hasilpengurangan=A-B
+hasilperkalian=A*B
+hasilpembagian=A/B
+hasilmodulus=A%B
+print ('hasil penjumlahan',hasilpenjumlahan)
+print ('hasil pengurangan',hasilpengurangan)
+print ('hasil pembagian',hasilpembagian)
+print ('hasil perkalian',hasilperkalian)
+print ('hasil modulus',hasilmodulus)
