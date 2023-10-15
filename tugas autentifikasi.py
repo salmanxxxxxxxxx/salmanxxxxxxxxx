997280304d593bfaf4c24bad6e2274329bb72949
5520123021 - SALMAN ALPARIZI <145079690+salmanxxxxxxxxx@users.noreply.github.com>
2023-10-15 16:03:50 +0800

    Add files via upload

---
+++ tugas autentifikasi.py
@@ -0,0 +1,77 @@
+Python 3.11.6 (tags/v3.11.6:8b6ee5b, Oct  2 2023, 14:57:12) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukan nama
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukan nama:salman alparizi
+masukan password:123
+username atau password salah
+
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukan nama:salman alparizi
+masukan password:12345678901234567890
+username atau password salah
+
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukan nama:salman alparizi
+masukan password:123456
+username atau password salah
+
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman
+masukkan password:123
+Traceback (most recent call last):
+  File "C:/Users/M_B Maxx/Documents/New folder/autentikasi.py", line 3, in <module>
+    code_username = salman
+NameError: name 'salman' is not defined
+
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman
+masukkan password:123
+Traceback (most recent call last):
+  File "C:/Users/M_B Maxx/Documents/New folder/autentikasi.py", line 3, in <module>
+    code_username = salman
+NameError: name 'salman' is not defined
+
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman
+masukkan password:123
+Traceback (most recent call last):
+  File "C:/Users/M_B Maxx/Documents/New folder/autentikasi.py", line 3, in <module>
+    code_username = salman
+NameError: name 'salman' is not defined
+
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman
+masukkan password:123
+Traceback (most recent call last):
+  File "C:/Users/M_B Maxx/Documents/New folder/autentikasi.py", line 3, in <module>
+    code_username = salman
+NameError: name 'salman' is not defined
+>>> 
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman
+masukkan password:1234
+Traceback (most recent call last):
+  File "C:/Users/M_B Maxx/Documents/New folder/autentikasi.py", line 3, in <module>
+    code_username = salman
+NameError: name 'salman' is not defined
+>>> 
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman 
+masukkan password:123
+Traceback (most recent call last):
+  File "C:/Users/M_B Maxx/Documents/New folder/autentikasi.py", line 3, in <module>
+    code_username = salman
+NameError: name 'salman' is not defined
+>>> 
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman
+masukkan password:123
+pasword atau username salah
+>>> 
+= RESTART: C:/Users/M_B Maxx/Documents/New folder/autentikasi.py
+masukkan username:salman
+masukkan password:123
+selamat datang
